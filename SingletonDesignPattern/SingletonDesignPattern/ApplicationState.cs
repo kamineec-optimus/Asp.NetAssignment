b6af189f6e2073a4fbe329c1c7ac214524b8f654
kamineec-optimus <kaminee.chaudhary@optimusinfo.com>
2014-12-30 15:00:45 +0530

    Singleton Pattern

---
+++ SingletonDesignPattern/SingletonDesignPattern/ApplicationState.cs
@@ -0,0 +1,43 @@
+﻿
+namespace SingletonDesignPattern
+{
+    /// <summary>
+    /// Class to demostrate singleton design pattern
+    /// </summary>
+    class ApplicationState
+    {
+        /// <summary>
+        /// instance of ApplicationState class
+        /// </summary>
+        private static ApplicationState instanceOfApplicationState = null;
+        /// <summary>
+        /// Properties having State information of object
+        /// </summary>
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+
+        /// <summary>
+        /// private constructor of ApplicationState class to prevent other classes to initiate other instances of this class
+        /// </summary>
+        private ApplicationState()
+        {
+        }
+        /// <summary>
+        /// return the instance of the ApplicationState class
+        /// </summary>
+        /// <returns>instance of ApplicationState</returns>
+        public static ApplicationState GetState()
+        {
+            /// check if the instance is null
+            if (ApplicationState.instanceOfApplicationState == null)
+            {
+                //initialize the intance of ApplicationState
+                instanceOfApplicationState = new ApplicationState();
+            }
+            //return the instance of ApplicationState
+            return instanceOfApplicationState;
+        }
+       
+
+    }
+}
---
+++ SingletonDesignPattern/SingletonDesignPattern/MainClass.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+namespace SingletonDesignPattern
+{
+    /// <summary>
+    /// user class to create instance of ApplicationState class
+    /// </summary>
+    public class MainClass
+    {
+        /// <summary>
+        /// private strings used to print state of singleton object
+        /// </summary>
+        private const string userID = "UserId: {0}";
+        private const string roleID = "RoleId: {0}";
+
+        static void Main(string[] args)
+        {
+            //object to call GetInstancesState method
+            MainClass user = new MainClass();
+            //print state of second instance
+            user.GetInstancesState();
+        }
+
+        /// <summary>
+        /// Method to get state of two instances of ApplicationState class
+        /// </summary>
+        public void GetInstancesState()
+        {
+            //first instance
+            ApplicationState objApplicationState1 = ApplicationState.GetState();            
+            objApplicationState1.UserId = 1;
+            objApplicationState1.RoleId = 2;
+            
+            //Second instance
+            ApplicationState objApplicationState2 = ApplicationState.GetState();
+            Console.WriteLine(userID, objApplicationState2.UserId);
+            Console.WriteLine(roleID, objApplicationState2.RoleId);
+            Console.ReadLine();
+        }
+    }
+}
