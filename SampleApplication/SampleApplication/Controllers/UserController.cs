3b5e8479d263f93b0ca8f16f06166166b2b900a7
kamineec-optimus <kaminee.chaudhary@optimusinfo.com>
2015-01-08 18:31:31 +0530

    Mvp and Mvc

---
+++ SampleApplication/SampleApplication/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+using SampleApplication.Models;
+using System.Text;
+
+namespace SampleApplication.Controllers
+{
+    public class UserController : Controller
+    {
+        /// <summary>
+        /// Strings used in program
+        /// </summary>
+        private const string name = "Your Name is : {0}</br/>";
+        private const string userName = "Your UserName is : {0}</br/>";
+        private const string emailId = "Your Email is :  {0}</br/>";        
+        /// <summary>
+        /// This controller will return a model of User Type
+        /// </summary>
+        /// <returns>View of type User model</returns>
+        public ActionResult GetUserInformation()
+        {
+            SampleApplication.Models.User model = new SampleApplication.Models.User();
+            return View(model);
+        }
+
+        /// <summary>
+        /// This method will display the user information
+        /// </summary>
+        /// <param name="model">Object of User class</param>
+        /// <returns>User information</returns>
+        [HttpPost]
+        public ActionResult GetUserInformation(User model)
+        {
+            StringBuilder userInformation = new StringBuilder();
+            userInformation.Append(string.Format(name, model.FirstName));
+            userInformation.Append(string.Format(userName, model.UserName));
+            userInformation.Append(string.Format(emailId, model.EmailId));
+            return Content(userInformation.ToString());
+        }
+    }
+}
---
+++ SampleApplication/SampleApplication/Models/User.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace SampleApplication.Models
+{
+    /// <summary>
+    /// User Class will have the user information
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Public properties
+        /// </summary>        
+        public string FirstName { get; set; }       
+        public string UserName { get; set; }        
+        public string Password { get; set; }        
+        public string EmailId { get; set; }
+    }
+}
